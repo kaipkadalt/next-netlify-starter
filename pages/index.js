6c0166412774a358ecd78699124bc5dea4dd5584
kaipkadalt <100677432+kaipkadalt@users.noreply.github.com>
2022-03-01 11:02:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,22 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <meta name="title" content="Naujienos, Žinios, Straipsniai">
+<meta name="description" content="Naujienos Lietuvoje, bei pasaulyje. Straipsniai įvairiomis temomis: technologijos, psichologija, dizainas, pasidaryk pats ir kita">
+<meta name="keywords" content="Naujienos, Žinios, Orai, TV programa, Straipsniai, Receptai">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="author" content="Darius">
         <link rel="icon" href="/favicon.ico" />
+    
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="NAUJIENOS, ŽINIOS, ORAI, TV PROGRAMA" />
+    
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Naujienos Lietuvoje, bei pasaulyje. Straipsniai įvairiomis temomis: technologijos, psichologija, dizainas, pasidaryk pats ir kita. Patiekalų receptai.
+    <a href="https://www.zin.lt" target="_blank" title="Žinios ir Naujienos">Žinios</a> and <a href="https://www.kaipkada.lt" target="_blank" title="Naujienos, Straipsniai ir TV programa">Naujienos</a>
         </p>
       </main>
 
